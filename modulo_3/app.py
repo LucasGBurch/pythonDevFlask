b5b61e018d011f526e3789c0c29ec4b3971f56f7
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-02-29 10:46:59 -0300

    Testes automatizados de Create
    USAR "python -m" antes de chamar o pytest arquivo.py!!!

--- modulo_3/app.py
+++ modulo_3/app.py
@@ -21,7 +21,7 @@ def create_task():
     tasks.append(new_task)
     # print(tasks)
     # Retorno em json, em vez da string direto
-    return jsonify({"message": "Nova tarefa criada com sucesso."})
+    return jsonify({"message": "Nova tarefa criada com sucesso.", "id": new_task.id})
 
 
 @app.route('/tasks', methods=['GET'])
