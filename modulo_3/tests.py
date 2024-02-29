70471e34506277739128e5e8ae57a628f8555ea2
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-02-29 14:08:01 -0300

    Testes automatizados para UPDATE E DELETE
    Cada um possui um teste para validar a execução da requisição e outro teste para confirmar se foi executado o comportamento esperado da função. No caso de update, que os valores tenham sido alterados; um get da resposta_json = payload. No do delete, um erro 404 ao tentar o get de uma tarefa que já foi deletada e não existe mais.

--- modulo_3/tests.py
+++ modulo_3/tests.py
@@ -1,11 +1,12 @@
 import pytest
 import requests
 
-# CRUD
+# TESTES PARA O CRUD
 BASE_URL = 'http://127.0.0.1:5000'
 tasks = []
 
 
+# CREATE
 def test_create_task():
     new_task_data = {
         "title": "Nova tarefa",
@@ -20,6 +21,7 @@ def test_create_task():
     tasks.append(response_json['id'])
 
 
+# READ ALL
 def test_get_tasks():
     response = requests.get(f"{BASE_URL}/tasks")
     assert response.status_code == 200
@@ -28,6 +30,7 @@ def test_get_tasks():
     assert "total_tasks" in responnse_json
 
 
+# READ ID
 def test_get_task():
     if tasks:
         task_id = tasks[0]
@@ -35,3 +38,40 @@ def test_get_task():
         assert response.status_code == 200
         response_json = response.json()
         assert task_id == response_json['id']
+
+
+# PUT
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "completed": True,
+            "description": "Nova descrição da tarefa",
+            "title": "Título atualizado",
+        }
+
+        # Atualização e retorno da mensagem da requisição
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        # Nova requisição a tarefa específica, para ver se os valores foram atualizados
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json['title'] == payload['title']
+        assert response_json['description'] == payload['description']
+        assert response_json['completed'] == payload['completed']
+
+
+# DELETE
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        response.status_code == 200
+
+        # Tarefa específica
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404  # Registro deve ser inexistente após deletar
