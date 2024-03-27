87f540d74844f4377b65ec60b183a01f359c6b8f
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-03-27 14:24:29 -0300

    Bug concertado e criação de rota logout e create
    Rotas para criação de usuários novos me fez encontrar o erro da tabela inexistente no banco de dados: Eu estava com uma dentro do módulo 4 onde eu criava user e outra na raiz do projeto. Tomar cuidado no futuro quando tiver mais de um projeto no mesmo repo e não perder essa noção dos diretórios.

--- modulo_4/app.py
+++ modulo_4/app.py
@@ -17,7 +17,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.session.query(User).filter_by(id=user_id).one_or_none()
+    return User.query.get(user_id)
 
 
 @app.route('/login', methods=["POST"])
@@ -43,6 +43,22 @@ def login():
 def logout():
     logout_user()
     return jsonify({"message": "Logout realizado com sucesso!"})  # 200
+
+
+@app.route('/user', methods=["POST"])
+@login_required
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso"}), 200
+
+    return jsonify({"message": "Dados inválidos"}), 401
 
 
 @app.route("/hello-world", methods=["GET"])
